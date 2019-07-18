13d731ac47f4c54608356a43baa47aee932172c3
Elijah Helmandollar <evh111@elijahs-air.attlocal.net>
2019-07-18 00:34:00 -0400

    Added nodemon and server script

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 const url =
-  'mongodb+srv://elijah:Vaughan10@cluster0-voeeo.mongodb.net/test?retryWrites=true&w=majority';
+  'mongodb+srv://evh111:Vaughan7@cluster0-voeeo.mongodb.net/test?retryWrites=true&w=majority';
 
 const connect = mongoose.connect(url, {
   useNewUrlParser: true,
